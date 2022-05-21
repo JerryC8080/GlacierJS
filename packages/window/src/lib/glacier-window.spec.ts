0b7a343280e6397d82d06560baed2adbe0662af9
jerrychuang <jerrychuang@tencent.com>
2022-05-21 23:55:18 +0800

    test: 💍 晚上 glaicer-window 单测

--- packages/window/src/lib/glacier-window.spec.ts
+++ packages/window/src/lib/glacier-window.spec.ts
@@ -1,30 +1,75 @@
+import { EventNames } from '@glacierjs/core';
 import { GlacierWindow, WindowPlugin } from '../index';
-import { Logger, LoggerLevel } from '@glacierjs/core';
 
-Logger.level = LoggerLevel.INFO;
+// mock workbox-window
+const workboxMock = {
+  register: jest.fn(),
+  messageSW: jest.fn().mockResolvedValue('test-message-return'),
+};
+jest.mock('workbox-window', () => ({
+  Workbox: jest.fn().mockImplementation(() => ({
+    register: (...args) => workboxMock.register(...args),
+    messageSW: (...args) => workboxMock.messageSW(...args),
+  }))
+}));
+
+// mock navigator.serviceWorker
+const serviceWokerMock = {
+  addEventListener: jest.fn(),
+  register: jest.fn(),
+};
+Object.defineProperty(global.navigator, 'serviceWorker', {
+  get() {
+    return serviceWokerMock;
+  }
+});
+
+afterAll(() => {
+  jest.restoreAllMocks();
+});
 
 describe('Glacier Window', () => {
-  Object.defineProperty(global.navigator, 'serviceWorker', {
-    value: {
-      addEventListener: jest.fn(),
-      register: jest.fn(),
-    },
-  });
 
-  test('should work', async () => {
+
+  test('should work fine', async () => {
     const glacierWindow = new GlacierWindow('./abc.sw');
 
     class MyPlugin implements WindowPlugin {
       name = 'test-plugin';
       onUse = jest.fn();
       beforeRegister = jest.fn();
+      onRedundant = jest.fn();
     }
 
     const myPlugin = new MyPlugin();
     glacierWindow.use(myPlugin);
 
     expect(myPlugin.onUse).toBeCalled();
-    await expect(glacierWindow.register()).rejects.toThrow();
+    await glacierWindow.register();
+    expect(workboxMock.register).toBeCalled();
     expect(myPlugin.beforeRegister).toBeCalled();
+
+    await glacierWindow.unregister();
+    expect(workboxMock.messageSW).toBeCalledWith({ type: EventNames.UN_INSTALL });
+    expect(myPlugin.onRedundant).toBeCalled();
+  });
+
+  test('should throw while register fail', async () => {
+    const error = new Error('test-error');
+    workboxMock.register.mockImplementationOnce(() => {
+      throw error;
+    });
+    const glacierWindow = new GlacierWindow('./abc.sw');
+    await expect(glacierWindow.register()).rejects.toEqual(error);
+  });
+
+  test('should throw while unregister fail', async () => {
+    const error = new Error('test-error');
+    workboxMock.messageSW.mockImplementationOnce(() => {
+      throw error;
+    });
+    const glacierWindow = new GlacierWindow('./abc.sw');
+    await glacierWindow.register();
+    await expect(glacierWindow.unregister()).rejects.toEqual(error);
   });
 });
